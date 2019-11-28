be6f1807577353ad302e37e843f2165b6ad7d748
WessonChen <hhchenweicheng@gmail.com>
2019-11-28 17:06:30 +1100

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { DocTitleChanger, CounterUseCounterHook, FormUseInputHook } from './hooks/CustomHook';
 
 class App extends Component {
     render() {
         return (
             <div className='app'>
-                <DocTitleChanger />
-                <CounterUseCounterHook />
-                <FormUseInputHook />
+                
             </div>
         );
     }
